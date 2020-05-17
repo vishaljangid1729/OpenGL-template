a273b80490883be1d883cd2cd61e42d6ff66994c
vishal jangid <jangidvishal1999@gmail.com>
2020-05-17 21:38:09 +0530

    shader red trinagle

--- main.cpp
+++ main.cpp
@@ -1,5 +1,45 @@
 #include "common.h"
 
+static unsigned int CompileShader( unsigned int type, const string& source) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if(result == GL_FALSE){
+        int lenght;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &lenght);
+        char* message = (char*)alloca(lenght*sizeof(char));
+        glGetShaderInfoLog(id, lenght, &lenght, message);
+        cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex":"fragment") << "Shader" << endl;
+        cout << message << endl;
+
+        glDeleteShader(id);
+        return 0;
+    }
+    return id;
+}
+
+static unsigned int CreateShader(const string& vertexShader, string &fragmentShader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+
+}
+
 int main(int argc, char *argv[])
 {
     if (SDL_Init(SDL_INIT_NOPARACHUTE) && SDL_Init(SDL_INIT_EVERYTHING) != 0)
@@ -13,7 +53,7 @@ int main(int argc, char *argv[])
 
     SDL_Window *window =
         SDL_CreateWindow("Hello window", SDL_WINDOWPOS_CENTERED,
-                         SDL_WINDOWPOS_CENTERED, 500, 500, SDL_WINDOW_OPENGL
+                         SDL_WINDOWPOS_CENTERED, 500, 500, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
 
         );
 
@@ -48,6 +88,25 @@ int main(int argc, char *argv[])
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, 0);
 
 
+    string vertexShader = 
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0 ) in vec4 position;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+    string fragmentShader = 
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0 ) out vec4 color;\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+
+    glUseProgram(shader);
 
 
     while (isRunning)
